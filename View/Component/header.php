234aeb19b513b4607c5790dbeb9e70a6ad68c73a
Pingumask <Pingumask@elanta.fr>
2021-03-16 07:56:28 +0100

    Update header.php

--- View/Component/header.php
+++ View/Component/header.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./Ressources/style.css">
-        <title>Document</title>
+        <title>Chat Ajax</title>
         <script src="./js/handleSubmit.js" defer></script>
         <script src="./js/getNewMessages.js"></script>
     </head>
